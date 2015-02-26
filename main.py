b20658f60862a9d69362e10fdf46d2143d22c1bd
Chris Reeves <hello@chris.reeves.io>
2015-02-26 16:04:53 +0000

    Redis PubSub controlled player

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import json
 import os
+import redis
 import spotify
 import sys
 import threading
@@ -9,7 +11,7 @@ class Player(object):
     def __init__(self):
         self.config = spotify.Config()
         self.config.load_application_key_file(
-            os.environ.get('SPOTIFY_APP_KEY'))
+            os.environ.get('SPOTIFY_KEY_PATH'))
         self.config.dont_save_metadata_for_playlists = True
         self.config.initially_unload_playlists = True
 
@@ -36,28 +38,52 @@ class Player(object):
         self.logged_in.wait()
 
     def play(self, uri):
-        print 'playing'
         track = self.session.get_track(uri).load()
 
         self.session.player.load(track)
         self.session.player.play()
 
-        while not self.end_of_track.wait(0.1):
-            pass
+    def pause(self):
+        self.session.player.pause()
+
+    def resume(self):
+        if self.session.player.state == spotify.PlayerState.PAUSED:
+            self.session.player.play()
+
+    def stop(self):
+        if self.session.player.state != spotify.PlayerState.UNLOADED:
+            self.session.player.unload()
 
     def on_connection_state_updated(self, session):
         if session.connection.state is spotify.ConnectionState.LOGGED_IN:
             print 'Logged In'
             self.logged_in.set()
 
-    def on_end_of_track(self):
+    def on_end_of_track(self, *agrs, **kwargs):
         print 'End of Track'
         self.end_of_track.set()
 
 
 if __name__ == '__main__':
+    r = redis.StrictRedis(
+        host=os.environ.get('REDIS_HOST'),
+        port=int(os.environ.get('REDIS_PORT')),
+        db=int(os.environ.get('REDIS_DB')))
+    pubsub = r.pubsub()
+    pubsub.subscribe('fm.player')
+    player = Player()
     try:
-        player = Player()
-        player.play('spotify:track:6xZtSE6xaBxmRozKA0F6TA')
+        while True:
+            for item in pubsub.listen():
+                if item.get('type') == 'message':
+                    data = json.loads(item.get('data'))
+                    if data['event'] == 'play':
+                        player.play(data['track'])
+                    if data['event'] == 'pause':
+                        player.pause()
+                    if data['event'] == 'resume':
+                        player.resume()
+                    if data['event'] == 'stop':
+                        player.stop()
     except KeyboardInterrupt:
         sys.exit()
