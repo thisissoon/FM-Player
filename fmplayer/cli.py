d85754128fb2df1bf964e29af478403be416323b
Chris Reeves <hello@chris.reeves.io>
2015-02-28 14:27:37 +0000

    Log level configuration in click

--- fmplayer/cli.py
+++ fmplayer/cli.py
@@ -9,20 +9,29 @@ CLI interface for FM Player.
 """
 
 import click
+import logging
 
 from fmplayer.player import Player
 
 
 @click.group()
-def cli():
+@click.option(
+    '--log-level',
+    '-l',
+    type=click.Choice(['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']),
+    default='ERROR')
+@click.pass_context
+def cli(ctx, log_level):
     """FM Player is the thisissoon.fm Player software.
     """
 
-    pass
+    ctx.obj['LOG_LEVEL'] = logging.getLevelName(log_level)
+    click.echo('Log Level: {0}'.format(log_level))
 
 
 @cli.command()
-def play():
+@click.pass_context
+def play(ctx):
     """ Start the Player
     """
 
