b76db5ebd35f7e3c46c73130a24de068ff48ce01
Chris Reeves <hello@chris.reeves.io>
2015-02-28 10:55:24 +0000

    Plays now plays track from a central redis data store

--- main.py
+++ main.py
@@ -33,11 +33,34 @@ class Player(object):
             os.environ.get('SPOTIFY_USER'),
             os.environ.get('SPOTIFY_PASS'))
 
+        self.redis = redis.StrictRedis(
+            host=os.environ.get('REDIS_HOST'),
+            port=int(os.environ.get('REDIS_PORT')),
+            db=int(os.environ.get('REDIS_DB')))
+
         self.logged_in = threading.Event()
         self.end_of_track = threading.Event()
         self.logged_in.wait()
 
+        self.listen()
+
+    def listen(self):
+        pubsub = self.redis.pubsub()
+        pubsub.subscribe('fm.player')
+
+        while True:
+            for item in pubsub.listen():
+                if item.get('type') == 'message':
+                    data = json.loads(item.get('data'))
+                    if data['event'] == 'pause':
+                        self.pause()
+                    if data['event'] == 'resume':
+                        self.resume()
+                    if data['event'] == 'stop':
+                        self.stop()
+
     def play(self, uri):
+        print 'Playing {0}'.format(uri)
         track = self.session.get_track(uri).load()
 
         self.session.player.load(track)
@@ -54,36 +77,40 @@ class Player(object):
         if self.session.player.state != spotify.PlayerState.UNLOADED:
             self.session.player.unload()
 
+    def watch_playlist(self):
+        print self.session.player.state
+        if self.session.player.state == spotify.PlayerState.UNLOADED:
+            print 'Watching playlist'
+            while True:
+                if self.redis.llen('playlist') > 0:
+                    print 'Playlist not empty - stopped watching'
+                    self.play(self.redis.lpop('playlist'))
+                    return
+
     def on_connection_state_updated(self, session):
         if session.connection.state is spotify.ConnectionState.LOGGED_IN:
             print 'Logged In'
             self.logged_in.set()
 
+            if self.redis.llen('playlist') > 0:
+                self.play(self.redis.lpop('playlist'))
+            else:
+                print 'Playlist empty'
+                self.watch_playlist()
+
     def on_end_of_track(self, *agrs, **kwargs):
         print 'End of Track'
-        self.end_of_track.set()
+        self.session.player.unload()
+        if self.redis.llen('playlist') > 0:
+            print 'playing next track'
+            self.play(self.redis.lpop('playlist'))
+        else:
+            print 'Playlist empty'
+            self.watch_playlist()
 
 
 if __name__ == '__main__':
-    r = redis.StrictRedis(
-        host=os.environ.get('REDIS_HOST'),
-        port=int(os.environ.get('REDIS_PORT')),
-        db=int(os.environ.get('REDIS_DB')))
-    pubsub = r.pubsub()
-    pubsub.subscribe('fm.player')
-    player = Player()
     try:
-        while True:
-            for item in pubsub.listen():
-                if item.get('type') == 'message':
-                    data = json.loads(item.get('data'))
-                    if data['event'] == 'play':
-                        player.play(data['track'])
-                    if data['event'] == 'pause':
-                        player.pause()
-                    if data['event'] == 'resume':
-                        player.resume()
-                    if data['event'] == 'stop':
-                        player.stop()
+        Player()
     except KeyboardInterrupt:
         sys.exit()
