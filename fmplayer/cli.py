cbf4a9db6b542ed3cd189dbd17f0cb3cca18ddee
Chris Reeves <hello@chris.reeves.io>
2015-03-11 12:08:51 +0000

    Set default volume and mute state

--- fmplayer/cli.py
+++ fmplayer/cli.py
@@ -98,6 +98,8 @@ def player(*args, **kwargs):
 
     # Create Handler Instance
     handler = EventHandler(redis, player, channel)
+    handler.set_volume({'volume': 70})  # Default volume
+    handler.set_mute({'mute': False})  # Default mute off
 
     # Threads - Queue and Event Watcher
     threads = [
